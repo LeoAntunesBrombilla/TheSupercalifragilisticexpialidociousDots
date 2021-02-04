0908127e49d5c7550d0949b18bf57a9ecc159614
kdav5758 <sebastiancadavid5758@gmail.com>
2021-02-04 17:43:50 -0500

    progressed on install.sh and added new .<files>

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # My Dotfiles :coffee:
-A collection of my personal dotfiles and scripts to install them in brand new machines that require configuration
+A collection of my personal dotfiles and scripts to install them in brand new machines that requires configuration
 
 ## About the repository
 This .dotfiles go from configuration files for vim, to even github and input profiles, all the file can be found in the dotfiles/ directory. Furthermore, this repository is still in development and the installation sripts are yet to be finalized.
--- install.sh
+++ install.sh
@@ -2,7 +2,20 @@
 
 #-------------------=== configs ===-------------------------------
 this_script=`basename "$0"`
+hashtag="#"
 installed_packages="apt_manual_list.txt"
+lines_packages=`wc -l $installed_packages | awk '{ print $1 }'`
+declare -A files=(
+    #["original_file_path"]="dotfiles_repo_copy"
+    ["~/.config/nvim/init.vim"]="dotfiles/vim/nvim/init.vim"
+    ["~/.vimrc"]="dotfiles/vim/vim/.vimrc"
+    ["~/.bashrc"]="dotfiles/shell/.bashrc"
+    ["~/.config/kitty/kitty.conf"]="dotfiles/kitty/kitty.conf"
+    ["~/.inputrc"]="dotfiles/user/.inputrc"
+    ["~/.profile"]="dotfiles/user/.profile"
+    ["~/.gitconfig"]="dotfiles/git/.gitconfig"
+)
+
 script_help=$( cat << EOF
 
 This script installs all many utils, packages and configuration used on my daily programming
@@ -18,21 +31,107 @@ EOF
 
 #-------------------=== script ===-------------------------------
 
-function update_files() {   #updates .dotfiles cat-ting current ones into the ones in the local repo
-    `cat ~/.vimrc > dotfiles/vim/.vimrc`; `cat ~/.config/nvim/init.vim > dotfiles/vim/init.vim`     #vim & nvim
-    `cat ~/.bashrc > dotfiles/shell/.bashrc`                                                        #.bashrc
-    `cat ~/.config/kitty/kitty.conf > dotfiles/kitty/kitty.conf`                                    #kitty
-    `cat ~/.inputrc > dotfiles/user/.inputrc`; `cat ~/.profile > dotfiles/user/.profile`            #user
+function ProgressBar() {
+    # Process data
+        let _progress=(${1}*100/${2}*100)/100
+        let _done=(${_progress}*4)/10
+        let _left=40-$_done
+    # Build progressbar string lengths
+        _fill=$(printf "%${_done}s")
+        _empty=$(printf "%${_left}s")
+
+    # 1.2 Build progressbar strings and print the ProgressBar line
+    # 1.2.1 Output example:                           
+    # 1.2.1.1 Progress : [########################################] 100%
+    printf "\rProgress : [${_fill// /$hashtag}${_empty// /-}] ${_progress}%%"
 }
 
+function update_files() {   #updates .dotfiles cat-ting current ones into the ones in the local repo
+
+    _end="${#files[@]}"
+    counter=1
+
+    for key in ${!files[@]}; do
+        # sleep 0.1
+        cat_it="cat ${key} > ${files[${key}]}"
+        eval $cat_it
+        ProgressBar ${counter} ${_end}
+        counter=$((counter+1))
+    done
+
+    echo ""
+
+    # `cat ~/.vimrc > dotfiles/vim/.vimrc`; `cat ~/.config/nvim/init.vim > dotfiles/vim/init.vim`     #vim & nvim
+    # `cat ~/.bashrc > dotfiles/shell/.bashrc`                                                        #.bashrc
+    # `cat ~/.config/kitty/kitty.conf > dotfiles/kitty/kitty.conf`                                    #kitty
+    # `cat ~/.inputrc > dotfiles/user/.inputrc`; `cat ~/.profile > dotfiles/user/.profile`            #user
+}
+
+function semylink_creator() {
+    :
+}
+
+function install_utils() {
+    read -t 10 -n 1 -s -r -p "'${lines_packages}' utilities will be installed/upgraded, press any key to continue"
+
+    counter=1
+
+    echo -e "\n\n\n###########################################################\n\n"
+
+    while read line; do
+        # echo "sudo apt-get install $line"
+        echo "💡----------Installing: $line"
+        ProgressBar ${counter} ${lines_packages}
+        echo -e "\n"
+
+        sudo apt-get install $line
+
+        sleep 0.1
+        echo -e "\n🎉'$line' was successfully installed/upgraded!🎉\n\n"
+        counter=$((counter+1))
+    done < $installed_packages
+
+    echo ""
+}
 
 if [[ -n "$1" ]]; then
     case "$1" in
         -h|--help)
             echo "$script_help"; exit 0
             ;;
-        -g|--generate-list) #list installed packages into a file
+        -gi|--generate-list) #list installed packages into a file
             `apt-mark showmanual > $installed_packages`
+            exit 0
+            ;;
+        -cs|--create-semylink)
+            semylink_creator
+            exit 0
+            ;;
+        -iu|--install-utils|--install-utilities)
+            time_start=`date +%s`
+            if [[ -f "$installed_packages" ]]; then
+                install_utils
+
+                time_end=`date +%s`
+                runtime=$((time_end-time_start))
+
+                case ${?} in
+                    0)
+                        echo -e "\n-----------------------------------------"
+                        echo -e "Everything was installed/upgraded successfully!"
+                        echo -e "Time: ${runtime}secs."
+                        ;;
+                    *)
+                        echo -e "\n\nAn error ocurred while installing/upgrading some utility/utilities, check '$this_script --help' for help."
+                        echo -e "Time: ${runtime}secs."
+                        ;;
+                esac
+
+            else
+                echo -e "File $installed_packages does not exists, make sure that you ran ./$this_script --generate-list first.\n"
+                exit 1
+            fi
+
             exit 0
             ;;
         -u|--update-files)

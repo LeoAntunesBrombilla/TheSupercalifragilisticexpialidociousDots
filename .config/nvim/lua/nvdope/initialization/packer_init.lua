277ba7f34cc4460d85c9fcf37785601b7654ed83
Pocco81 <pocco451@gmail.com>
2021-08-05 13:32:22 -0500

    better packer installer

--- .config/nvim/lua/nvdope/initialization/packer_init.lua
+++ .config/nvim/lua/nvdope/initialization/packer_init.lua
@@ -1,30 +1,43 @@
-vim.cmd("packadd packer.nvim")
+local cmd = vim.cmd
+
+cmd("packadd packer.nvim")
 local present, packer = pcall(require, "packer")
 
 if not present then
+	local fn = vim.fn
+	local o = vim.o
     local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 
-    print("Cloning packer..")
-    -- remove the dir before cloning
     vim.fn.delete(packer_path, "rf")
-    vim.fn.system(
-        {
-            "git",
-            "clone",
-            "https://github.com/wbthomason/packer.nvim",
-            "--depth",
-            "20",
-            packer_path
-        }
-    )
 
-    vim.cmd("packadd packer.nvim")
+	local installer_scipt = [[
+		git clone https://github.com/wbthomason/packer.nvim --depth 20 ]] .. packer_path .. [[
+	]]
+
+	local function onExit(_, code)
+		if code ~= 0 then
+			error("Failed to install packer.nvim!")
+		end
+		print("Successfully installed packer.nvim")
+	end
+
+	cmd("new")
+	local shell = o.shell
+	o.shell = "/bin/bash"
+
+	fn.termopen("set -e\n" .. installer_scipt, {["cwd"] = packer_path, ["on_exit"] = onExit})
+	o.shell = shell
+
+	cmd("startinsert")
+
+
+    cmd("packadd packer.nvim")
     present, packer = pcall(require, "packer")
 
     if present then
-        print("Packer cloned successfully.")
+        print("Packer.nvim fully installed and required")
     else
-        error("Couldn't clone packer !\nPacker path: " .. packer_path)
+        error("Couldn't require packer.nvim!")
     end
 end
 
